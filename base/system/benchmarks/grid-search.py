d680aa916b77150d8dd55fee10d8cbc9a2858def
Byron Zhong <byronzhong@cs.uchicago.edu>
2024-12-03 15:23:53 -0600

    add grid search scripts

---
+++ base/system/benchmarks/grid-search.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import json
+from itertools import product
+from datetime import datetime
+from concurrent.futures import (
+    ThreadPoolExecutor,
+    as_completed,
+)
+from rich.progress import (
+    BarColumn,
+    MofNCompleteColumn,
+    Progress,
+    TextColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
+
+booleans = ["true", "false"]
+
+PARAMETERS = {
+    "flatten-selfref": booleans,
+    # "flatten-liberally": booleans,
+    # "sharing-dist-cutoff": range(1, 5),
+    # "sharing-size-cutoff": range(2, 10),
+    # "sharing-use-cutoff": range(1, 5),
+}
+
+PROGRAMS = [
+    # 'hamlet2',
+    # 'vliw',
+    # 'barnes-hut',
+    'life',
+    # 'mc-ray',
+    # 'lexgen',
+    # 'mandelbrot',
+    # 'mandelbrot-int',
+    # 'nucleic',
+]
+
+def get_grid(parameters):
+    keys, values = parameters.keys(), parameters.values()
+    combinations = product(*values)
+    return enumerate(dict(zip(keys, combo)) for combo in combinations)
+
+def get_program_param(programs, grid):
+    return product(programs, grid)
+
+def test_param(program, param):
+    param_id, param_dict = param
+    param_list = [f"-Cnc.{flag}={value}" for flag, value in param_dict.items()]
+    param_list.append("-Cnc.flatten-reg-limit=false")
+
+    process = subprocess.run(
+        ["./test.sh", program] + param_list,
+        check=True,
+        capture_output=True,
+        text=True,
+    )
+    result = eval(process.stdout)
+    progress.advance(total)
+    return (param_id, result)
+
+grid = list(get_grid(PARAMETERS))
+all_tasks = list(get_program_param(PROGRAMS, grid))
+
+progress = Progress(
+    TextColumn("[progress.description]{task.description}"),
+    BarColumn(),
+    MofNCompleteColumn(),
+    TimeElapsedColumn(),
+    TimeRemainingColumn(),
+)
+
+results = {}
+with progress, ThreadPoolExecutor(max_workers=1) as pool:
+    total = progress.add_task("All", total=len(all_tasks))
+    futures = []
+    for program, param in all_tasks:
+        fut = pool.submit(test_param, program, param)
+        futures.append(fut)
+
+    for fut in as_completed(futures):
+        param_id, result = fut.result()
+        results[param_id] = result
+
+report = {
+    "params": dict(grid),
+    "results": results,
+}
+
+result_filename = datetime.now().strftime("gridsearch_%Y%m%d_%H%M%S.json")
+
+try:
+    with open(result_filename, "w") as result_file:
+        json.dump(report, result_file)
+except Exception as e:
+    print(json.dumps(results))
+    raise
+
---
+++ base/system/benchmarks/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SML='../../../bin/sml'
+NRUNS=10
+
+prog="$1"
+shift
+out_file=$(mktemp "$prog-XXXXX")
+flags=$@
+
+echo "{\"bmark\" : \"$prog\", \"flags\":\"$@\", " > $out_file
+$SML $flags <<EOF > /dev/null 2>&1
+  use "timeit.sml";
+  use "$prog.sml";
+  val outS = TextIO.openAppend("$out_file");
+  Timing.time($NRUNS, outS, Main.doit);
+  TextIO.flushOut outS;
+  Measuring.measure(outS, Main.doit);
+  TextIO.flushOut outS;
+  TextIO.closeOut outS;
+EOF
+echo "}" >> $out_file
+
+cat $out_file
+rm $out_file
