8a339165712992346579b4e038f2bcff01aa91f9
Byron Zhong <34432561+zbyrn@users.noreply.github.com>
2025-02-03 14:57:18 -0600

    better presets script

--- base/system/benchmarks/presets.sh
+++ base/system/benchmarks/presets.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
+flags=""
+
 if [ x"$1" = "x--new" ] ; then
-  echo "Cnc.enable=true -Cnc.flatten-reg-limit=false"
+  flags="-Cnc.enable=true -Cnc.flatten-reg-limit=false"
+  shift
 elif [ x"$1" = "x--reg-limit" ] ; then
-  echo "-Cnc.enable=true"
+  flags="-Cnc.enable=true"
+  shift
 elif [ x"$1" = "x--no-flatten" ] ; then
-  echo "-Cnc.enable=true -Cnc.flatten-policy=0"
+  flags="-Cnc.enable=true -Cnc.flatten-policy=0"
+  shift
 elif [ x"$1" = "x--flat-closure" ] ; then
-  echo "-Cnc.enable=true -Cnc.sharing-size-cutoff=100000 -Cnc.sharing-no-thinning=true -Cnc.flatten-policy=0"
+  flags="-Cnc.enable=true -Cnc.sharing-size-cutoff=100000 -Cnc.sharing-no-thinning=true -Cnc.flatten-policy=0"
+  shift
 elif [ x"$1" = "x--conservative" ] ; then
-  echo "-Cnc.enable=true -Cnc.flatten-liberally=false"
+  flags="-Cnc.enable=true -Cnc.flatten-liberally=false"
+  shift
 elif [ x"$1" = "x--no-active-sharing" ] ; then
-  echo "-Cnc.enable=true -Cnc.sharing-size-cutoff=100000"
+  flags="-Cnc.enable=true -Cnc.sharing-size-cutoff=100000"
+  shift
 elif [ x"$1" = "x--no-sharing" ] ; then
-  echo "-Cnc.enable=true -Cnc.sharing-size-cutoff=100000 -Cnc.sharing-no-thinning=true"
+  flags="-Cnc.enable=true -Cnc.sharing-size-cutoff=100000 -Cnc.sharing-no-thinning=true"
+  shift
 elif [ x"$1" = "x--time" ] ; then
-  echo "-Cnc.enable=true -Cnc.flatten-reg-limit=false -Cnc.sharing-size-cutoff=6 -Cnc.sharing-dist-cutoff=1 -Cnc.sharing-use-cutoff=1 -Cnc.flatten-selfref=true -Cnc.flatten-liberally=false"
+  flags="-Cnc.enable=true -Cnc.flatten-reg-limit=false -Cnc.sharing-size-cutoff=6 -Cnc.sharing-dist-cutoff=1 -Cnc.sharing-use-cutoff=1 -Cnc.flatten-selfref=true -Cnc.flatten-liberally=false"
+  shift
 elif [ x"$1" = "x--space" ] ; then
-  echo "-Cnc.enable=true -Cnc.flatten-reg-limit=true -Cnc.sharing-size-cutoff=3 -Cnc.sharing-dist-cutoff=1 -Cnc.sharing-use-cutoff=1 -Cnc.flatten-selfref=false -Cnc.flatten-liberally=true"
-else
-  echo $@
+  flags="-Cnc.enable=true -Cnc.flatten-reg-limit=true -Cnc.sharing-size-cutoff=3 -Cnc.sharing-dist-cutoff=1 -Cnc.sharing-use-cutoff=1 -Cnc.flatten-selfref=false -Cnc.flatten-liberally=true"
+  shift
 fi
+
+flags="$flags $@"
+
+echo $flags
